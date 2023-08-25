3ea3b35b80abc3c9b824f63561e2cfe245b4f3a5
Guiller24 <94213163+Guiller24@users.noreply.github.com>
2023-08-25 19:40:03 +0800

    Delete bloggering/blog-website-java directory

--- bloggering/blog-website-java/src/main/java/com/blogback/blogwebsitejava/repositories/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.blogback.blogwebsitejava.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.blogback.blogwebsitejava.models.Posts;
-
-public interface PostRepository extends JpaRepository<Posts, Integer> {
-    
-}
